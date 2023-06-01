8df169f6807bd4e842847ccef694afe47c35ba11
41023146 <41023146@gm.nfu.edu.tw>
2023-06-01 23:18:23 +0800

    添加球員程式頁面

--- downloads/zmq_football/player.py
+++ downloads/zmq_football/player.py
@@ -15,56 +15,60 @@ print('Simulation started')
 
 player='/a_player1'
 v=8
+a=40
 
-
-def setBubbleRobVelocity(leftWheelVelocity1, rightWheelVelocity1,leftWheelVelocity2, rightWheelVelocity2):
+def setVelocity(lfwV, rfwV,lbwV, rbwV):
     leftMotor1 = sim.getObject(player+'/joint_lf')
     rightMotor1 = sim.getObject(player+'/joint_rf')
     leftMotor2 = sim.getObject(player+'/joint_lb')
     rightMotor2 = sim.getObject(player+'/joint_rb')
-    sim.setJointTargetVelocity(leftMotor1, leftWheelVelocity1)
-    sim.setJointTargetVelocity(rightMotor1, rightWheelVelocity1)
-    sim.setJointTargetVelocity(leftMotor2, leftWheelVelocity2)
-    sim.setJointTargetVelocity(rightMotor2, rightWheelVelocity2)
+    sim.setJointTargetVelocity(leftMotor1, lfwV)
+    sim.setJointTargetVelocity(rightMotor1, rfwV)
+    sim.setJointTargetVelocity(leftMotor2, lbwV)
+    sim.setJointTargetVelocity(rightMotor2, rbwV)
     #輸入四個變數分別給四個軸速度
     
-def setBubbleRobangel(a):
-    brickRob= sim.getObject(player)
-    angel = [-90*math.pi/180, a*math.pi/180, 0*math.pi/180]
+def setangel(y):
+    ontology= sim.getObject(player)
+    angel = [-90*math.pi/180, y*math.pi/180, 0*math.pi/180]
     leftMotor = sim.getObject(player+'/joint_lf')
     rightMotor = sim.getObject(player+'/joint_rf')
-    sim.setObjectOrientation(leftMotor, brickRob, angel)
-    sim.setObjectOrientation(rightMotor, brickRob, angel)
+    sim.setObjectOrientation(leftMotor, ontology, angel)
+    sim.setObjectOrientation(rightMotor, ontology, angel)
     #輸入一個變數改變前輪方向
-  
-while True:
+
+
+
+def playercontrol(x,y):
     if keyboard.is_pressed('w'):
-        setBubbleRobVelocity(v,v,v,v)
+        setVelocity(x,x,x,x)
         if keyboard.is_pressed('a'):
-            setBubbleRobangel(-40)
+            setangel(-y)
         elif keyboard.is_pressed('d'):
-            setBubbleRobangel(40)
+            setangel(y)
         else:
-            setBubbleRobangel(0)
+            setangel(0)
     elif keyboard.is_pressed('s'):
-        setBubbleRobVelocity(-v,-v,-v,-v)
+        setVelocity(-x,-x,-x,-x)
         if keyboard.is_pressed('a'):
-            setBubbleRobangel(-40)
+            setangel(-y)
         elif keyboard.is_pressed('d'):
-            setBubbleRobangel(40)
+            setangel(y)
         else:
-            setBubbleRobangel(0)
+            setangel(0)
     elif keyboard.is_pressed('a'):
-        setBubbleRobVelocity(-v,v,-v,v)
+        setVelocity(-x,x,-x,x)
     elif keyboard.is_pressed('d'):
-        setBubbleRobVelocity(v,-v,v,-v)
+        setVelocity(x,-x,x,-x)
     elif keyboard.is_pressed('q'):
         # stop simulation
         sim.stopSimulation()
     else:
-        setBubbleRobVelocity(0, 0, 0, 0)
-        setBubbleRobangel(0)
-
-
-
-
+        setVelocity(0, 0, 0, 0)
+        setangel(0)
+  
+while True:
+    if keyboard.is_pressed('shift'):
+        playercontrol(v+4,a-20)
+    else:
+        playercontrol(v,a)
