9d7d57d563b0f0281347a0b432269ad913c55205
41023146 <41023146@gm.nfu.edu.tw>
2023-06-13 20:56:15 +0800

    add player_auto.py

---
+++ downloads/zmq_football/player_auto.py
@@ -0,0 +1,110 @@
+# pip install pyzmq cbor keyboard
+from zmqRemoteApi import RemoteAPIClient
+from zmqRemoteApi_IPv6 import RemoteAPIClient
+import keyboard
+import random
+import math
+
+
+
+client = RemoteAPIClient('localhost', 23000)
+#client = RemoteAPIClient('2001:288:6004:17:2023:cda:4:6', 23000)
+
+print('Program started')
+sim = client.getObject('sim')
+#sim.startSimulation()
+print('Simulation started')
+
+v=8
+a=40
+d=[0,0,0]
+
+#選擇控制的球員
+player='/a_player1'
+#player='/a_player2'
+#player='/a_player3'
+#player='/a_player4'
+#player='/b_player1'
+#player='/b_player2'
+#player='/b_player3'
+#player='/b_player4'
+
+
+
+def setVelocity(lfwV, rfwV,lbwV, rbwV):
+    leftMotor1 = sim.getObject(player+'/joint_lf')
+    rightMotor1 = sim.getObject(player+'/joint_rf')
+    leftMotor2 = sim.getObject(player+'/joint_lb')
+    rightMotor2 = sim.getObject(player+'/joint_rb')
+    sim.setJointTargetVelocity(leftMotor1, lfwV)
+    sim.setJointTargetVelocity(rightMotor1, rfwV)
+    sim.setJointTargetVelocity(leftMotor2, lbwV)
+    sim.setJointTargetVelocity(rightMotor2, rbwV)
+    #輸入四個變數分別給四個軸速度
+    
+def setangel(y):
+    ontology= sim.getObject(player)
+    angel = [-90*math.pi/180, y*math.pi/180, 0*math.pi/180]
+    leftMotor = sim.getObject(player+'/joint_lf')
+    rightMotor = sim.getObject(player+'/joint_rf')
+    sim.setObjectOrientation(leftMotor, ontology, angel)
+    sim.setObjectOrientation(rightMotor, ontology, angel)
+    #輸入一個變數改變前輪方向
+def controlangel(y,a,f):
+    if a[2]<f:
+        setangel(-y)
+    elif a[2]>f:
+        setangel(y)
+    else:
+        setangel(0)
+def turnover():
+    floor= sim.getObject('/Floor')
+    player1 = sim.getObject(player)
+    a=sim.getObjectOrientation(player1,floor)
+    b=sim.getObjectPosition(player1,floor)
+    a[0]=0
+    a[1]=0
+    b[2]=0.3
+    sim.setObjectPosition(player1,floor,b)
+    sim.setObjectOrientation(player1,floor,a)
+def playercontrol(x,y):
+    floor= sim.getObject('/Floor')
+    ball=sim.getObject('/Sphere')
+    player1 = sim.getObject(player)
+    a1=sim.getObjectOrientation(player1,floor)
+    b=sim.getObjectPosition(player1,floor)
+    c=sim.getObjectPosition(ball,floor)
+    
+    d[0]=c[0]-b[0]
+    d[1]=c[1]-b[1]
+    print(d)
+    e=(d[0]**2+d[1]**2)**0.5
+    f=math.asin(d[1]/e)
+    print(a1[2])
+    print(f)
+   
+    if a1[2]<(f+20*math.pi/180) and (a1[2]>f-20*math.pi/180):
+        print("0")
+        setVelocity(x,x,x,x)
+        controlangel(y,a1,f)
+    elif keyboard.is_pressed('s'):
+        setVelocity(-x,-x,-x,-x)
+        controlangel(y)
+    elif keyboard.is_pressed('a'):
+        setVelocity(-x,x,-x,x)
+    elif keyboard.is_pressed('d'):
+        setVelocity(x,-x,x,-x)
+    elif keyboard.is_pressed('space'):
+        turnover()
+    elif keyboard.is_pressed('q'):
+        # stop simulation
+        sim.stopSimulation()
+    else:
+        setVelocity(0, 0, 0, 0)
+        setangel(0)
+  
+while True:
+    if keyboard.is_pressed('shift'):
+        playercontrol(v+4,a-20)
+    else:
+        playercontrol(v,a)
